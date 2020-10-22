d9e16c7e9afb19d60e6898b26f76885009800796
jackie <jackie.ju@gmail.com>
2020-10-22 18:00:02 +0800

    c

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
     def abap2ruby
+        code = params[:code]
+        code = "" if !code
+        code = code.stripe
+        
         render :text=>"OK"
     end
     def cpp2ruby
